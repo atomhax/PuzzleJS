2b47efcff27f01295f1a4a35f615b74162b6291a
BEng1 <beng1@asu.edu>
2015-12-15 18:37:53 -0700

    Music now loops

--- PuzzelJS/Scripts/Sounds.js
+++ PuzzelJS/Scripts/Sounds.js
@@ -5,19 +5,23 @@ function Sounds(option) {
     
   
     if (option == "music") {
-        var music = new Audio("/Content/Files/Music.mp3");
+        music = new Audio('/Content/Files/Music.mp3');
+        music.addEventListener('ended', function () {
+            this.currentTime = 0;
+            this.play();
+        }, false);
         music.play();
     }
     if (option == "swap") {
-        var swap = new Audio("/Content/Files/Swap.mp3");
+        swap = new Audio("/Content/Files/Swap.mp3");
         swap.play();
     }
     if (option == "clear") {
-        var clear = new Audio("/Content/Files/clear.ogg");
+        clear = new Audio("/Content/Files/clear.ogg");
         clear.play();
     }
     if (option == "drop") {
-        var drop = new Audio("/Content/Files/drop.ogg");
+        drop = new Audio("/Content/Files/drop.ogg");
         drop.play();
     }
 
